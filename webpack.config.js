7bc8ddac63ca454d8d25b76b16fa30ee92bd83df
radioflygit <radiofly19841018@gmail.com>
2017-11-22 15:37:05 +0800

    搭个本地环境,添加一个小玩意

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+//定义了一些文件夹的路径
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'src/scripts');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+module.exports = {
+  //项目的文件夹 可以直接用文件夹名称 默认会找index.js 也可以确定是哪个文件名字
+  entry: APP_PATH,
+  //输出的文件名 合并以后的js会命名为bundle.js
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.js'
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  }
+};
